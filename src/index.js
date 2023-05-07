41c93fed57993b2f2c22148ba6e8ba70e28f7dd6
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-07 04:27:52 -0500

    add parcel build

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ mongoose.connect('mongodb://mongo:27017/app', {
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const Item = require('./models/Item');
+  const Item = require('../src/models/Item');
 
 app.get('/', (req, res) => {
   console.log("hit home route");
   
   Item.find()
-    .then(items => res.render('index', { items }))
+    .then(items => res.render('../src/views/index', { items }))
     .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
 
